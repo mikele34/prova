418705eecfa699a5d28c9c4314f976fe650cc25b
mikele34 <82645362+mikele34@users.noreply.github.com>
2021-05-24 15:36:11 +0200

    Turret Menu

--- TowerDefense/Assets/Script/EndlessWaveSpawner.cs
+++ TowerDefense/Assets/Script/EndlessWaveSpawner.cs
@@ -17,20 +17,9 @@ public class EndlessWaveSpawner : MonoBehaviour
 
     void Update()
     {
-        if (EnemiesAlive > 0)
-        {
-            return;
-        }
-
-        if (waveIndex == waves.Length)
-        {
-            gameManager.WinLevel();
-            this.enabled = false;
-        }
 
         if (coutdown <= 0)
         {
-            StartCoroutine(SpawnWave());
             coutdown = timeBetweenWaves;
             return;
         }
@@ -42,22 +31,6 @@ public class EndlessWaveSpawner : MonoBehaviour
         WaveCountdownText.text = string.Format("{0:00.00}", coutdown);
     }
 
-    IEnumerator SpawnWave()
-    {
-        PlayerStats.Rounds++;
-
-        Wave wave = waves[waveIndex];
-
-        EnemiesAlive = wave.count;
-
-        for (int i = 0; i < wave.count; i++)
-        {
-            SpawnEnemy(wave.enemy);
-            yield return new WaitForSeconds(1f / wave.rate);
-        }
-
-        waveIndex++;
-    }
 
     void SpawnEnemy(GameObject enemy)
     {
